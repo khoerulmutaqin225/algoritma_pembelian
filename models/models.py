310d10d3b2abf0fb3412b057c087679a87b16440
khoerulmutaqinkm <92683033+khoerulmutaqinkm@users.noreply.github.com>
2023-06-08 12:25:40 +0700

    Relasi Many2many atau Tags

--- models/models.py
+++ models/models.py
@@ -4,10 +4,16 @@ class algoritma_pembelian(models.Model):
     _name = 'algoritma.pembelian'
 
     name = fields.Char(string="Name")
+
     tanggal = fields.Date(string="Tanggal")
     status = fields.Selection([('draft','Draft'),('to_approve','To Approve'),('approved','Approved'),('done','Done')], default='draft')
     algoritma_pembelian_ids = fields.One2many('algoritma.pembelian.line', 'algoritma_pembelian_id', string="Algoritma Pembelian Ids")
-
+    brand_ids = fields.Many2many(
+        'algoritma.brand',
+        'algoritma_pembelian_brand_rel',
+        'algoritma_pembelian_id',
+        'brand_id',
+        string='Brand Ids')
 
 class algoritma_pembelian_line(models.Model):
     _name = 'algoritma.pembelian.line'
@@ -17,6 +23,10 @@ class algoritma_pembelian_line(models.Model):
     quantity = fields.Float(string="Quantity", default=0.0)
     uom_id = fields.Many2one('uom.uom', string="Uom Id")
 
+class algoritma_brand(models.Model):
+    _name = 'algoritma.brand'
+
+    name = fields.Char(string="Name")
 # -*- coding: utf-8 -*-
 
 #from odoo import models, fields, _
